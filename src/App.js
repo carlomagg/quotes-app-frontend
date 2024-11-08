80ec9b477e575d5a91ce6390fac4ab6577d65d8a
carlomagg <obijurumagnus@gmail.com>
2024-11-08 17:15:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   // Function to fetch a random quote from the backend
   const fetchQuote = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/quote');
+      const response = await axios.get('https://quotes-app-backend-exh9.onrender.com/quote');
       setQuote(response.data);
     } catch (error) {
       console.error('Error fetching quote:', error);
